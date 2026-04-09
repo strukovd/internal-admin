a944806bd961a0991d4eac969bf3a1afab4b1452
strukovd <dmitryi97@yandex.ru>
2026-04-09 19:31:37 +0600

    added default layout and page

---
+++ app/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="default-layout">
+    <slot />
+  </section>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss">
+.default-layout {
+
+}
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+	<section class="index-page">
+
+	</section>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss">
+.index-page {
+
+}
+</style>
