3803f960590e9d93f803be7e52a49d1536e11c70
strukovd <dmitryi97@yandex.ru>
2026-04-10 20:41:52 +0600

    fix @mdi, scss, global styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+	ssr: false,
+	devServer: {
+		host: '0.0.0.0',
+		port: 3000,
+	},
+	compatibilityDate: '2025-07-15',
+	devtools: { enabled: true },
+	css: [
+		'@mdi/font/css/materialdesignicons.min.css',
+		'~/assets/styles/global.scss'
+	],
 })
