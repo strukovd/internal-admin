8cd6dd907b8dffc7f227743b59c1196de71e16df
strukovd <dmitryi97@yandex.ru>
2026-04-29 21:23:37 +0600

    dasdsa

---
+++ app/middleware/auth.global.ts
@@ -0,0 +1,78 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+	// На сервере пропускаем (нет localStorage). Проверять будем на клиенте.
+	if (import.meta.server) return;
+
+	const user = useUserStore();
+	const NEEDS_AUTH = Boolean(to.meta.auth);
+	const ALLOWED_ROLES = Array.isArray(to.meta.roles) ? (to.meta.roles as string[]) : null;
+
+	// Если авторизация требуется
+	if(NEEDS_AUTH) {
+		if(!user.token) { // Если нет токена
+			return navigateTo({
+				path: '/login',
+				// query: { redirect: to.fullPath }
+			}, { replace: true })
+		}
+		else { // Если есть токен 🪙
+			const role = user.userData?.role;
+			if(ALLOWED_ROLES && ALLOWED_ROLES.length > 0) {
+				if(!ALLOWED_ROLES.includes(role)) { // Если пользователь не иммет доступа (роли) к странице
+					const page = determinePageByRole(role);
+					return navigateTo(page, { replace: true });
+				}
+			}
+
+		}
+	}
+
+	// Если уже авторизован и пришёл на /login — перекинуть на главную
+	if (to.path.replace(/\/$/, '') === '/login' && user.token) {
+		const role = user.userData?.role;
+		const page = determinePageByRole(role);
+		return navigateTo(page, { replace: true });
+	}
+
+
+
+
+	// 1) Если нужна авторизация, но её нет — на /login с возвратом после логина
+	// if (pageReqAuth && !user.token) {
+	// 	return navigateTo({
+	// 		path: '/login',
+	// 		query: { redirect: to.fullPath }
+	// 	}, { replace: true })
+	// }
+
+	// // 2) Если авторизован, но роль не подходит — на /403 или куда сочтёте
+	// if (pageReqAuth && allowedRoles && allowedRoles.length > 0) {
+	// 	console.log(user.userData?.role);
+
+	// 	if (!allowedRoles.includes(user.userData?.role)) {
+	// 	// return navigateTo('/403', { replace: true })
+	// 	}
+	// }
+
+	// // 3) Опционально: если уже авторизован и пришёл на /login — перекинуть на главную
+	// if (to.path === '/login' && user.token) {
+	// 	console.log(user);
+	// 	const role = user.userData?.role;
+	// 	return navigateTo('/', { replace: true });
+	// }
+})
+
+const determinePageByRole = (role: string): string => {
+	switch( String(role).toUpperCase() ) {
+		case 'CONTRACTOR':
+			return '/issues';
+		case 'ADMIN':
+			return '/';
+		case 'CONTROLLER':
+		case 'CALLCENTER':
+			return '/readings';
+
+		// Если токен не ошибочный и role не определена, или role неизвестна и не соответствует никакой странице
+		default:
+			return '/403';
+	}
+}
---
+++ app/pages/403.vue
@@ -0,0 +1,49 @@
+<template>
+	<section class="page-403">
+		<h1 class="h1">💀 403</h1>
+		<p class="description">Вы пытались зайти на страницу на которую у вас нет доступа.</p>
+		<p class="description">
+			<BaseButton color="#333" @click="navigateTo('/')" prependIcon="mdi-home">Перейдите на главную</BaseButton><br> для переадресации на страницу назначения,
+			или <br>
+			<BaseButton color="#333" @click="logout" prependIcon="mdi-logout"> сбросьте сессию</BaseButton>
+			<br> для повторной авторизации
+		</p>
+	</section>
+</template>
+
+<script lang="ts" setup>
+import BaseButton from '~/components/common/base/BaseButton.vue';
+
+definePageMeta({
+	// layout: 'authorized'
+});
+
+function logout() {
+	useUserStore().reset();
+	navigateTo('/login');
+}
+</script>
+
+<style lang="scss">
+
+body:has(.page-403) {
+	background-color: #0079C1;
+	color: #fff;
+}
+
+.page-403 {
+	.base-button {
+		font-size: .8em;
+		margin: 1em 0;
+	}
+	.h1 {
+		font-size: 6em;
+		text-align: center;
+	}
+	.description {
+		font-size: 1.4em;
+		line-height: 1em;
+		text-align: center;
+	}
+}
+</style>
