bce4e7d70a8eaa94912a76214093de15bff19d28
strukovd <dmitryi97@yandex.ru>
2026-04-29 17:17:57 +0600

    GAZ-1926: added sidebar component and layout

---
+++ app/components/Sidebar.vue
@@ -0,0 +1,281 @@
+<template>
+	<section class="sidebar">
+		<ul class="links">
+			<li
+				v-for="link of links"
+				:key="link.link ?? link.title ?? link.class"
+				class="link"
+			>
+				<div v-if="link.spacer" :class="link.class">{{ link.title ?? '' }}</div>
+				<template v-else>
+					<NuxtLink
+						:to="link.link"
+						:class="['link-row', { 'active': isActiveLink(link), 'disabled': link.disabled }]"
+						@click="handleDisabledLink(link, $event)"
+					>
+						<BaseIcon :name="link.icon!" size="20" style="margin-right:.6em;"/>
+						{{ link.title }}
+					</NuxtLink>
+					<ul v-if="link.children?.length" class="children">
+						<li
+							v-for="child of link.children"
+							:key="child.link ?? child.title"
+							class="child-link"
+						>
+							<div v-if="child.spacer" :class="child.class">{{ child.title ?? '' }}</div>
+							<NuxtLink v-else
+								:to="child.link"
+								:class="['link-row', { 'active': isActiveLink(child), 'disabled': child.disabled }]"
+								@click="handleDisabledLink(child, $event)"
+							>
+								<BaseIcon :name="child.icon!" size="18" style="margin-right:.6em;"/>
+								{{ child.title }}
+							</NuxtLink>
+						</li>
+					</ul>
+				</template>
+			</li>
+		</ul>
+	</section>
+</template>
+
+<script lang="ts" setup>
+import BaseIcon from './common/base/BaseIcon.vue';
+
+type SidebarLink = {
+	title?: string
+	link?: string
+	icon?: string
+	disabled?: boolean
+	spacer?: boolean
+	class?: string
+	children?: SidebarLink[]
+}
+
+const route = useRoute();
+
+const links = ref<SidebarLink[]>([
+	{
+		title: 'Панель управления',
+		link: '/dashboard',
+		icon: 'mdi-view-dashboard'
+	},
+	{
+		title: 'Абоненты',
+		link: '/clients',
+		icon: 'mdi-account-group',
+		disabled: true,
+		children: [
+			{
+				title: 'Поиск абонентов',
+				link: '/clients',
+				icon: 'mdi-account-group',
+				disabled: true
+			},
+			{
+				title: 'Чат',
+				link: '/chat',
+				icon: 'mdi-chat',
+				disabled: true,
+				// Возможность переотправки сообщений
+			},
+			{
+				title: 'Баланс',
+				link: '/balance',
+				icon: 'mdi-wallet',
+				disabled: true
+			}
+		]
+	},
+		{
+		title: 'Юрики',
+		icon: 'mdi-scale-balance',
+		disabled: true,
+		children: [
+			{
+				title: 'Поиск абонентов',
+				link: '/counterparty',
+				icon: 'mdi-account-search',
+				disabled: true
+			},
+			{
+				spacer: true,
+				class: 'splitter',
+			},
+			{
+				title: 'Загрузка абонентов',
+				link: '/counterparty/upload',
+				icon: 'mdi-account-multiple-plus',
+				// Возможность переотправки сообщений
+			},
+			{
+				title: 'Загрузка балансов',
+				link: '/counterparty/upload-balances',
+				icon: 'mdi-wallet',
+			},
+			{
+				title: 'Загрузка показаний',
+				link: '/counterparty/upload-readings',
+				icon: 'mdi-counter',
+			}
+		]
+	},
+	{
+		title: 'Тарифы',
+		link: '/tariffs',
+		icon: 'mdi-currency-usd',
+		disabled: true
+	},
+	{
+		title: 'Массовая замена',
+		link: '/mass-replacement',
+		icon: 'mdi-account-multiple',
+		disabled: true
+	},
+	{
+		title: 'Онлайн квитанции',
+		link: '/invoices',
+		icon: 'mdi-invoice-text-multiple',
+		disabled: true
+	},
+
+	{
+		title: 'Портал',
+		link: '/portal',
+		icon: 'mdi-web',
+		disabled: true
+	},
+
+	{
+		spacer: true,
+		class: 'splitter',
+	},
+	{
+		title: 'Маршруты',
+		link: '/trips',
+		icon: 'mdi-map-marker',
+		disabled: true,
+		// Возможность менять даты
+	},
+
+	{
+		spacer: true,
+		class: 'splitter',
+	},
+	{
+		title: 'Жалобы',
+		link: '/complaints',
+		icon: 'mdi-alert-circle',
+		disabled: true
+	},
+	{
+		title: 'Ответственные лица',
+		link: '/responsibles',
+		icon: 'mdi-account-group',
+		disabled: true
+	},
+	{
+		title: 'FAQ',
+		link: '/faq',
+		icon: 'mdi-help-circle-outline',
+		disabled: true
+	},
+
+	{
+		spacer: true,
+		class: 'spacer',
+	},
+	{
+		title: 'Настройки',
+		link: '/settings',
+		icon: 'mdi-cog',
+		disabled: true
+	},
+	{
+		title: 'Выход',
+		link: '/logout',
+		icon: 'mdi-logout',
+		disabled: true
+	}
+]);
+
+function isActiveLink(link: SidebarLink) {
+	if( link.link && route.path === link.link ) return true;
+	return Boolean(link.children?.some(isActiveLink));
+}
+
+function handleDisabledLink(link: SidebarLink, event: Event) {
+	if( !link.disabled ) return;
+	event.preventDefault();
+}
+</script>
+
+<style lang="scss">
+.sidebar {
+	display: flex;
+	flex-direction: column;
+	background:#0e3896;
+	color: #a9c0f6;
+	max-width:50vw;
+
+	.links {
+		display: flex;
+		flex-direction: column;
+		flex: 1 1 auto;
+		min-height: 0;
+		box-sizing: border-box;
+		list-style-type: none;
+		margin:0;
+		padding:1rem;
+
+		.link {
+			line-height: 3em;
+			border-radius:7px;
+			&:has(.spacer) {
+				flex:auto 1 1;
+			}
+			.splitter {
+				margin:.2em 0;
+				border-bottom:1px dashed #5082ee66;
+			}
+
+			.link-row {
+				display: block;
+				color: inherit;
+				text-decoration: none;
+				font-size:1.1rem;
+				padding:0 1em;
+
+				&:hover,
+				&.active {
+					background:#124bc6;
+					color:#ffffff;
+					border-radius:7px;
+				}
+
+				&.disabled {
+					color:#ffffff55;
+					cursor: inherit;
+					pointer-events: none;
+				}
+			}
+
+			.children {
+				list-style-type: none;
+				margin:0;
+				padding:0 0 .4em 1.9em;
+
+				.child-link {
+					line-height: 2.4em;
+					border-radius:7px;
+
+					.link-row {
+						font-size:1rem;
+						padding:0 1em;
+					}
+				}
+			}
+		}
+	}
+}
+</style>
---
+++ app/layouts/sidebar.vue
@@ -0,0 +1,113 @@
+<template>
+	<section class="sidebar-layout">
+		<header class="sidebar-header" style="display:flex; align-items:center; gap:1em; padding:.5em 2em;">
+			<section class="logo" style="display: flex; align-items:center; gap:.6em; margin-right:2em;">
+				<img height="20px" src="/favicon.svg" alt="Логотип" />
+				<h2>Internal Admin</h2>
+			</section>
+			<section class="search">
+				<BaseTextBox ref="searchInput" placeholder="Поиск абонента по ФИО, лицевому счету" prependIcon="mdi-magnify" />
+			</section>
+			<section class="actions" style="flex:auto 1 0; display:flex; gap:.5em;">
+
+			</section>
+			<section style="display:flex; gap:1em;">
+				<BaseIcon name="mdi-bell" size="20" style="color:#525252;"/>
+				<BaseIcon name="mdi-help-circle-outline" size="20" style="color:#525252;"/>
+				<BaseIcon name="mdi-cog" size="20" style="color:#525252;"/>
+			</section>
+			<section class="user-box" style="display:flex; align-items:center; gap:.5em;">
+				<!-- <img src="/img/user-avatar.png" alt="Аватар пользователя" /> -->
+				<div class="user-info" style="text-align:right;">
+					<div class="name" style="color:#171717; font-size:.9rem; font-weight:700;">Admin</div>
+					<div class="role" style="color:#737373; font-size:.8rem;">Администратор</div>
+				</div>
+				<div>
+					<div style="background: #2563ea; color:#feffff; padding:.3em; border-radius:50%; font-weight: 700; line-height:2em; width:2em; aspect-ratio:1/1; text-align:center;">А</div>
+				</div>
+				<BaseIcon @click="logout" name="mdi-logout" size="20" style="color:#a3a3a3;"/>
+			</section>
+		</header>
+		<main class="sidebar-main">
+			<Sidebar v-resizeble="`right`"/>
+			<section class="page-content">
+				<slot/>
+			</section>
+		</main>
+	</section>
+</template>
+
+<script lang="ts" setup>
+import BaseButton from '~/components/common/base/BaseButton.vue';
+import BaseIcon from '~/components/common/base/BaseIcon.vue';
+import BaseTextBox from '~/components/common/base/BaseTextBox.vue';
+import Sidebar from '~/components/Sidebar.vue';
+
+const userStore = useUserStore();
+const searchInput = ref<any>(null);
+// if( !userStore.userData ) navigateTo('/login');
+
+onMounted(() => {
+	enableAutofocus();
+});
+
+function logout() {
+	userStore.reset();
+	navigateTo('/login');
+}
+
+function enableAutofocus() {
+	window.addEventListener('keydown', (e) => {
+		if( !searchInput.value ) return;
+		const input = searchInput.value.$el.querySelector('input') as HTMLInputElement;
+
+		// 1. Проверяем, что фокус НЕ в другом текстовом поле
+		const isInput = ['INPUT', 'TEXTAREA', 'SELECT'].includes(document.activeElement?.tagName) || document.activeElement?.isContentEditable;
+
+		// 2. Игнорируем системные сочетания (Ctrl+C, Alt+Tab, F5 и т.д.)
+		const isModifier = e.ctrlKey || e.metaKey || e.altKey;
+
+		// 3. Проверяем, что нажата печатаемая клавиша (буква, цифра или спецсимвол)
+		// В современных браузерах e.key.length === 1 гарантирует, что это символ
+		if (!isInput && !isModifier && e.key.length === 1) {
+			input.focus();
+			// Фокус перейдет, но символ напечатается автоматически благодаря событию
+		}
+
+		// Дополнительно: очистка поиска по Escape
+		if (e.key === 'Escape' && document.activeElement === input) {
+			input.setAttribute('value', '');
+			input.blur(); // Убираем фокус
+		}
+	});
+}
+
+</script>
+
+<style lang="scss">
+.sidebar-layout {
+	min-height: 100dvh;
+	display: flex;
+	flex-direction: column;
+
+	.sidebar-header {
+		// занимает свою естественную высоту
+		flex: 0 0 auto;
+		background-color: #f6f7fb;
+		border-bottom: 1px solid #e5e5e5;
+	}
+
+	.sidebar-main {
+		// забирает остаток экрана
+		display: flex;
+		flex: 1 1 auto;
+		min-height: 0; // можно быть меньше высоты собственного контента, если layout требует, позволяет дочерним элементам корректно обрабатывать overflow
+
+		.page-content {
+			flex: auto 1 1;
+			overflow-y: auto;
+			padding: 2em;
+		}
+	}
+}
+</style>
