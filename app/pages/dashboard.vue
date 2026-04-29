e1137a6e5fa9d546599d662ac799b09d5f695164
strukovd <dmitryi97@yandex.ru>
2026-04-29 17:17:15 +0600

    GAZ-1926: added main pages

---
+++ app/pages/dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+	<section class="dashboard-page">
+
+	</section>
+</template>
+
+<script lang="ts" setup>
+import BaseIcon from '~/components/common/base/BaseIcon.vue';
+import BaseIsland from '~/components/common/base/BaseIsland.vue';
+
+definePageMeta({
+	// auth: true,
+	// roles: ['ADMIN', 'MANAGER'],
+	layout: 'sidebar'
+});
+</script>
+
+<style lang="scss">
+.dashboard-page {
+
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang="ts" setup>
-
+navigateTo('/dashboard');
 </script>
 
 <style lang="scss">
